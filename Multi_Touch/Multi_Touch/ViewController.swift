c3910aff0a79e4149140c33dcf3ad9c14fcad78c
stven1004 <stven100482@gmail.com>
2019-05-21 15:26:36 +0900

    add code

--- Multi_Touch/Multi_Touch/ViewController.swift
+++ Multi_Touch/Multi_Touch/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var interactionSwitch: UISwitch!
+    
+    @IBOutlet weak var multiTouchSwitch: UISwitch!
+    
+    @IBOutlet weak var touchView: UIView!
+    
+    
+    @IBAction func switchInteractionEnabled(_ sender: UISwitch) {
+        touchView.isUserInteractionEnabled = sender.isOn
+    }
+    
+    @IBAction func switchMultiTouch(_ sender: UISwitch) {
+        touchView.isUserInteractionEnabled = sender.isOn
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        interactionSwitch.isOn = touchView.isUserInteractionEnabled
+        multiTouchSwitch.isOn = touchView.isMultipleTouchEnabled
     }
 
 
